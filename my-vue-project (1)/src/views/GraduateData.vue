8adce3fd2e385456d0cb15afa79aa6b35f85a81c
wtig-z <18718467433@163.com>
2024-10-23 19:12:56 +0800

    GraduateData侧边栏的迁移
    GraduateData侧边栏的迁移

--- my-vue-project (1)/src/views/GraduateData.vue
+++ my-vue-project (1)/src/views/GraduateData.vue
@@ -182,10 +182,10 @@
             <!-- 插槽：内容页面可以在这里填充具体内容 -->
              <div class="gchart">
               <slot name="content">
-            
-              
-            <GraduatesChart/>
-             
+            <!-- <GraduatesChart/> -->
+            <div class="gchart1">
+      <Pie :data="chartData" :options="chartOptions" />
+    </div>
              
         
          
@@ -200,16 +200,62 @@
   </template>
   
   <script>
+//迁移
+import { Pie } from 'vue-chartjs'
+  import { Chart, ArcElement, Tooltip, Legend } from 'chart.js'
+  
+  // 注册所需的 Chart.js 组件
+  Chart.register(ArcElement, Tooltip, Legend)
+  
+
+
+
   import GraduatesChart from '../components/GraduatesChart.vue';
   export default {
     // name:MainLayout,
+    name: 'GraduatesChart',
     components:{
-        GraduatesChart
+        GraduatesChart,
+        Pie
+       
     },
     data() {
       return {
+       
         isLightMode: false,
         isBlack: true,
+
+        chartData: {
+          labels: ['网络工程', '信息管理与信息系统'],
+          datasets: [
+            {
+              label: '毕业生人数',
+              backgroundColor: ['#36A2EB', '#FF6384'],
+              data: [296, 138]
+            }
+          ]
+        },
+        chartOptions: {
+          responsive: true,
+          plugins: {
+            legend: {
+              display: true,
+              position: 'top'
+            },
+            title: {
+              display: true,
+              text: '网络技术系毕业生数据'
+            },
+            tooltip: {
+              callbacks: {
+                label: function(context) {
+                  return `${context.label}: ${context.raw} 人 (${((context.raw / 434) * 100).toFixed(2)}%)`
+                }
+              }
+            }
+          }
+        }
+      
       };
     },
     methods: {
